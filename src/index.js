25878253c9d9984e450bd1ace1255a70450beacb
Julia <diniullova2011@yandex.ru>
2021-08-18 15:58:01 +0400

    fix int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let val = n.toString().split('').reverse().join('');
+
+  if (val.endsWith('-')) {
+      return parseInt(val);
+  }
+  else {
+    val = '-' + val;
+    return parseInt(val);
+  }
+    
 }
